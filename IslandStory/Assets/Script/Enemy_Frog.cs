9f5ac6d9da66506af3820402b89e6bdc8c3b7264
shanexuu89 <shanexu89@hotmail.com>
2020-05-22 21:44:10 +1200

    Update_monster

---
+++ IslandStory/Assets/Script/Enemy_Frog.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Frog : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public Transform leftpoint, rightpoint;
+    public float Speed;
+    private float leftx, rightx;
+
+    private bool Faceleft = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        transform.DetachChildren();
+        leftx = leftpoint.position.x;
+        rightx = rightpoint.position.x;
+        Destroy(leftpoint.gameObject);
+        Destroy(rightpoint.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        
+    }
+
+    void Movement()
+    {
+        if(Faceleft)
+        {
+            rb.velocity = new Vector2(-Speed,rb.velocity.y);
+            if (transform.position.x < leftx)
+            {
+                transform.localScale = new Vector3(-1,1,1);
+                Faceleft = false;
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(Speed, rb.velocity.y);
+            if (transform.position.x > rightx)
+            {
+                transform.localScale = new Vector3(1,1,1);
+                Faceleft = true;
+            }
+        }
+
+
+    }
+
+
+}
