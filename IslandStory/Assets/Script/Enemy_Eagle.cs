72882ed6a55bf34ffee7bc6c1d35ad9bfeaa71fd
shanexuu89 <shanexu89@hotmail.com>
2020-05-23 01:19:03 +1200

    Update_monster_02

---
+++ IslandStory/Assets/Script/Enemy_Eagle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Eagle : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Collider2D coll;
+    public Transform top, bottom;
+    public float Speed;
+    private float TopY, BottomY;
+
+    private bool isUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+        TopY = top.position.y;
+        BottomY = bottom.position.y;
+        Destroy(top.gameObject);
+        Destroy(bottom.gameObject);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        if (isUp)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Speed);
+            if (transform.position.y > TopY)
+            {
+                isUp = false;
+            }
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -Speed);
+            if (transform.position.y < BottomY)
+            {
+                isUp = true;
+            }
+        }
+    }
+}
