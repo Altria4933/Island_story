53a0b7ec9b71b0f3a901433f77c4076bc81ced42
Altria4933 <junchen4933@gmail.com>
2020-05-24 00:25:38 +1200

    Added a death menu
    improved attack range as well

--- IslandStory/Assets/Sunnyland/artwork/Sprites/player/Bandits - Pixel Art/Demo/Bandit.cs
+++ IslandStory/Assets/Sunnyland/artwork/Sprites/player/Bandits - Pixel Art/Demo/Bandit.cs
@@ -2,18 +2,20 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class Bandit : MonoBehaviour {
+public class Bandit : MonoBehaviour
+{
 
-    [SerializeField] float      m_speed = 5.0f;
-    [SerializeField] float      m_jumpForce = 6.0f;
+    [SerializeField] float m_speed = 5.0f;
+    [SerializeField] float m_jumpForce = 6.0f;
 
-    private Animator            m_animator;
-    private Rigidbody2D         m_body2d;
-    private Sensor_Bandit       m_groundSensor;
-    private bool                m_grounded = false;
-    private bool                m_combatIdle = false;
+    private Animator m_animator;
+    private Rigidbody2D m_body2d;
+    private Sensor_Bandit m_groundSensor;
+    private bool m_grounded = false;
+    private bool m_combatIdle = false;
     private bool                m_isDead = false;
     
+
     public int maxHealth = 100;
     public int currentHealth;
     public HealthBar healthBar;
@@ -24,31 +26,36 @@ public class Bandit : MonoBehaviour {
     float atkRate = 2f;
     float nextAttackTime = 0f;
     //public attack m_attack;
-    
+
 
     public int Cherry = 0;
     public Text CherryNum;
 
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+        Time.timeScale = 1f;
         m_animator = GetComponent<Animator>();
         m_body2d = GetComponent<Rigidbody2D>();
         m_groundSensor = transform.Find("GroundSensor").GetComponent<Sensor_Bandit>();
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         //Check if character just landed on the ground
-        if (!m_grounded && m_groundSensor.State()) {
+        if (!m_grounded && m_groundSensor.State())
+        {
             m_grounded = true;
             m_animator.SetBool("Grounded", m_grounded);
         }
 
         //Check if character just started falling
-        if(m_grounded && !m_groundSensor.State()) {
+        if (m_grounded && !m_groundSensor.State())
+        {
             m_grounded = false;
             m_animator.SetBool("Grounded", m_grounded);
         }
@@ -70,15 +77,16 @@ public class Bandit : MonoBehaviour {
 
         // -- Handle Animations --
         //Death
-        if (currentHealth <=0)
+        if (currentHealth <= 0)
         {
-            
+            if (m_isDead == false)
+            {
                 m_animator.SetTrigger("Death");
+                m_isDead = true;
                 Debug.Log("You dead");
-
                 Time.timeScale = 0f;
-            currentHealth = 1;//for debug only
-    
+                FindObjectOfType<MenuBehaviour>().endGame();
+            }
         }
         //keep the souce code for debug 
         /*if (Input.GetKeyDown("e")) {
@@ -95,16 +103,17 @@ public class Bandit : MonoBehaviour {
             m_animator.SetTrigger("Hurt");
 
         //Attack
-        else if(Input.GetKeyDown("z")) {
+        else if (Input.GetKeyDown("z"))
+        {
             //Attack timer so the player wont go crazy
-            if(Time.time >= nextAttackTime)
+            if (Time.time >= nextAttackTime)
             {
                 attack();
                 nextAttackTime = Time.time + 1f / atkRate;
 
             }
             //m_attack.Update();
-            
+
         }
         /*
         else if(Input.GetMouseButtonDown(0)) {
@@ -117,7 +126,8 @@ public class Bandit : MonoBehaviour {
             m_combatIdle = !m_combatIdle;
 
         //Jump
-        else if (Input.GetKeyDown("space") && m_grounded) {
+        else if (Input.GetKeyDown("space") && m_grounded)
+        {
             m_animator.SetTrigger("Jump");
             m_grounded = false;
             m_animator.SetBool("Grounded", m_grounded);
@@ -125,14 +135,15 @@ public class Bandit : MonoBehaviour {
             m_groundSensor.Disable(0.2f);
         }
 
-         else if (Input.GetKeyDown("space") && m_grounded) {
+        else if (Input.GetKeyDown("space") && m_grounded)
+        {
             m_animator.SetTrigger("Jump");
             m_grounded = false;
             m_animator.SetBool("Grounded", m_grounded);
             m_body2d.velocity = new Vector2(m_body2d.velocity.x, m_jumpForce);
             m_groundSensor.Disable(0.2f);
         }
-         
+
 
         /* else if (Input.GetKeyDown("space") && coll.IsTouchingLayers(ground))
          {
@@ -146,7 +157,7 @@ public class Bandit : MonoBehaviour {
         //Combat Idle
         else if (m_combatIdle)
             m_animator.SetInteger("AnimState", 1);
-        
+
 
         else if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -174,7 +185,7 @@ public class Bandit : MonoBehaviour {
 
     private void OnDrawGizmosSelected()
     {
-        if(attackPoint == null)
+        if (attackPoint == null)
         {
             return;
         }
@@ -192,21 +203,26 @@ public class Bandit : MonoBehaviour {
     {
         if (collision.tag == "Collection")
         {
-         Destroy(collision.gameObject);
-         Cherry += 1;
-         CherryNum.text = Cherry.ToString();
+            Destroy(collision.gameObject);
+            Cherry += 1;
+            CherryNum.text = Cherry.ToString();
         }
     }
 
     //Eliminate Enemies
     private void onCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Enemies")
+        if (collision.gameObject.tag == "Enemies")
         {
             Destroy(collision.gameObject);
         }
-        
+
     }
 
     
+
+
+
 }
+
+
